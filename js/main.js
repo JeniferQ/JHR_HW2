af885558bf17c5595df990c34585443b7340f616
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2025-03-14 18:04:35 -0400

    Add games to Console class
    In this commit, the games array was added to the Console class. This way, many games can be added to a Console object.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
-import { Console, PlayStation5 } from "./console.js";
+import { Console, upgradedConsole } from "./console.js";
 
-const PS1 = new Console('PlayStation - PS1', '1994', 'history_text', 'feature_text');
+const PS1 = new Console('PlayStation', '1994', 'history_text', 'feature_text', 'accessory_text', ['Final Fantasy VII', 'Metal Gear Solid', 'Crash Bandicoot']);
 PS1.info();
+PS1.gameInfo();
 console.log(PS1);
 
-const PS2 = new Console('PlayStation 2 - PS1', '2000', 'history_text', 'feature_text');
+const PS2 = new Console('PlayStation 2', '2000', 'history_text', 'feature_text', 'accessory_text', ['Grand Theft Auto', 'Shadow of the Colossums', 'God of War II']);
 PS2.info();
+PS2.gameInfo();
 console.log(PS2);
 
-const PSP = new Console('PlayStation Portable - PSP', '2004', 'history_text', 'feature_text');
-PSP.info();
-console.log(PSP);
-
-const PS3 = new Console('PlayStation 3 - PS1', '2006', 'history_text', 'feature_text');
+const PS3 = new Console('PlayStation 3', '2006', 'history_text', 'feature_text', 'accessory_text', ['The Last of Us', 'Uncharted 2: Among Thieves', 'Metal Gear Solid 4: Guns of the Patriots']);
 PS3.info();
+PS3.gameInfo();
 console.log(PS3);
 
-const PSVITA = new Console('PlayStation Vita - PS Vita', '2011', 'history_text', 'feature_text');
-PSVITA.info();
-console.log(PSVITA);
 
-const PS4 = new Console('PlayStation 4 - PS4', '2013', 'history_text', 'feature_text');
+const PS4 = new Console('PlayStation 4', '2013', 'history_text', 'feature_text', 'accessory_text', ['God of War', "Marvel's Spider-Man", 'The Last of Us Part II']);
 PS4.info();
+PS4.gameInfo();
 console.log(PS4);
 
-const PS4PRO = new Console('PlayStation 4 Pro - PS4 Pro', '2016', 'history_text', 'feature_text');
-PS4PRO.info();
-console.log(PS4PRO);
 
-const PS5 = new PlayStation5('PlayStation 5 - PS5', '2020', 'history_text', 'feature_text', 'real-time ray tracing');
+const PS5 = new upgradedConsole('PlayStation 5', '2020', 'history_text', 'feature_text', 'accessory_text', ["Demon's Souls", 'Ratchet & Clank: Rift Apart', "Marvel’s Spider-Man 2"], 'real-time ray tracing');
 PS5.info();
-PS5.extra();
+PS5.gameInfo();
+PS5.extraInfo();
 console.log(PS5);
