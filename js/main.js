0fe50f63b0fb6776c36e412c740efc784016a0d4
Henrique Gamborgi <henriquegamborgi@gmail.com>
2025-03-16 19:50:24 -0400

    Update Page Outline

--- js/main.js
+++ js/main.js
@@ -1,29 +1,72 @@
+import { initHeader } from "./header.js";
 import { Console, upgradedConsole } from "./console.js";
 
-const PS1 = new Console('PlayStation', '1994', 'history_text', 'feature_text', 'accessory_text', ['Final Fantasy VII', 'Metal Gear Solid', 'Crash Bandicoot']);
+console.log("Javascripf file is connected");
+
+const PS1 = new Console(
+  "PlayStation",
+  "1994",
+  "history_text",
+  "feature_text",
+  "accessory_text",
+  ["Final Fantasy VII", "Metal Gear Solid", "Crash Bandicoot"]
+);
 PS1.info();
 PS1.gameInfo();
 console.log(PS1);
 
-const PS2 = new Console('PlayStation 2', '2000', 'history_text', 'feature_text', 'accessory_text', ['Grand Theft Auto', 'Shadow of the Colossums', 'God of War II']);
+const PS2 = new Console(
+  "PlayStation 2",
+  "2000",
+  "history_text",
+  "feature_text",
+  "accessory_text",
+  ["Grand Theft Auto", "Shadow of the Colossums", "God of War II"]
+);
 PS2.info();
 PS2.gameInfo();
 console.log(PS2);
 
-const PS3 = new Console('PlayStation 3', '2006', 'history_text', 'feature_text', 'accessory_text', ['The Last of Us', 'Uncharted 2: Among Thieves', 'Metal Gear Solid 4: Guns of the Patriots']);
+const PS3 = new Console(
+  "PlayStation 3",
+  "2006",
+  "history_text",
+  "feature_text",
+  "accessory_text",
+  [
+    "The Last of Us",
+    "Uncharted 2: Among Thieves",
+    "Metal Gear Solid 4: Guns of the Patriots",
+  ]
+);
 PS3.info();
 PS3.gameInfo();
 console.log(PS3);
 
-
-const PS4 = new Console('PlayStation 4', '2013', 'history_text', 'feature_text', 'accessory_text', ['God of War', "Marvel's Spider-Man", 'The Last of Us Part II']);
+const PS4 = new Console(
+  "PlayStation 4",
+  "2013",
+  "history_text",
+  "feature_text",
+  "accessory_text",
+  ["God of War", "Marvel's Spider-Man", "The Last of Us Part II"]
+);
 PS4.info();
 PS4.gameInfo();
 console.log(PS4);
 
-
-const PS5 = new upgradedConsole('PlayStation 5', '2020', 'history_text', 'feature_text', 'accessory_text', ["Demon's Souls", 'Ratchet & Clank: Rift Apart', "Marvel’s Spider-Man 2"], 'real-time ray tracing');
+const PS5 = new upgradedConsole(
+  "PlayStation 5",
+  "2020",
+  "history_text",
+  "feature_text",
+  "accessory_text",
+  ["Demon's Souls", "Ratchet & Clank: Rift Apart", "Marvel’s Spider-Man 2"],
+  "real-time ray tracing"
+);
 PS5.info();
 PS5.gameInfo();
 PS5.extraInfo();
 console.log(PS5);
+
+initHeader();
